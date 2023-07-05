9f95052c11afb696e390f77fc50fa513da2774fd
Guillermo Riaño <guilledark17@gmail.com>
2023-07-05 12:40:27 -0600

    ✅ Consult Method

--- WCF_ServiceApplication_1/IPerson.cs
+++ WCF_ServiceApplication_1/IPerson.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using WCF_ServiceApplication_1.Model;
 
 namespace WCF_ServiceApplication_1
 {
@@ -14,6 +15,7 @@ namespace WCF_ServiceApplication_1
     {
 
         [OperationContract]
+        IEnumerable<Person> Consult();
         
     }
 
--- WCF_ServiceApplication_1/PersonService.svc.cs
+++ WCF_ServiceApplication_1/PersonService.svc.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using WCF_ServiceApplication_1.Model;
 
 namespace WCF_ServiceApplication_1
 {
@@ -12,6 +13,15 @@ namespace WCF_ServiceApplication_1
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class PersonService : IPerson
     {
-        
+
+        // Consult Method
+        public IEnumerable<Person> Consult()
+        {
+            // Get Persons (List)
+            using (DatabaseEntities context = new DatabaseEntities())
+            {
+                return context.People.AsNoTracking().ToList();
+            }
+        }
     }
 }
