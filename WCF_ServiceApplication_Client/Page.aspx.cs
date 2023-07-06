8d92686fc8203834ae5893a6f4a90406a9bc630e
Guillermo Riaño <guilledark17@gmail.com>
2023-07-06 11:38:06 -0600

    ✅ Consult Method

---
+++ WCF_ServiceApplication_Client/Page.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WCF_ServiceApplication_Client
+{
+    public partial class Page : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Consult Method
+        private void Consult()
+        {
+            using (ServiceReference1.PersonClient client = new PersonClient())
+            {
+                grid.DataSource = client.Consult();
+                grid.DataBind();
+            }
+        }
+
+        protected void btnconsult_Click(object sender, EventArgs e)
+        {
+            Consult();
+        }
+    }
+}
